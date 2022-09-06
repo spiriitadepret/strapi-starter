9f3cb4038dff46591c3a65b5ddb8ea290f564e23
Florent Merlat <f.merlat@choosit.com>
2022-09-06 18:53:03 +0200

    Content types init

---
+++ src/api/actualite/controllers/actualite.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * actualite controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::actualite.actualite');
